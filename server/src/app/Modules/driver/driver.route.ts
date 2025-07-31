547fc9d0f30c1d1f1f9f815aadd6056c88d8c939
Saidul Islam Rana <codewithsaidul@gmail.com>
2025-08-01 00:37:33 +0600

    Update driver.route.ts

--- server/src/app/Modules/driver/driver.route.ts
+++ server/src/app/Modules/driver/driver.route.ts
@@ -1,13 +1,1 @@
-import { Router } from "express";
-import { checkAuth } from "../../middleware/checkAuth";
-import { Role } from "../user/user.interface";
-import { validateRequest } from "../../middleware/validateRequest";
-import {
-  driverApplicationZodSchema,
-  updateDriveAvailityStatusZodSchema,
-  updateDriverApplicationStatusSchema,
-} from "./driver.validation";
-import { DriverController } from "./driver.controller";
-
-const router = Router();
 
